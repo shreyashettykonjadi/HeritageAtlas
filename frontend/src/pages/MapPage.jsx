e0ff0c4ec0840a74ca33847e873d9c252f3392c2
shreyaShetty <shreyashetty8d@gmail.com>
2026-02-15 21:48:39 +0530

    added functionality to add id to header

--- frontend/src/pages/MapPage.jsx
+++ frontend/src/pages/MapPage.jsx
@@ -1,9 +1,28 @@
+import api from "../services/api"
+
 export default function MapPage() {
+
+  async function testRequest() {
+    try {
+      const res = await api.get("/")
+      console.log(res.data)
+    } catch (err) {
+      console.log(err.response?.data || err.message)
+    }
+  }
+
   return (
     <div className="p-10">
       <h1 className="text-3xl font-semibold">
         Map Page
       </h1>
+
+      <button
+        onClick={testRequest}
+        className="mt-6 px-4 py-2 bg-green-800 text-white rounded"
+      >
+        Test API
+      </button>
     </div>
   )
 }
---
+++ frontend/src/services/api.js
@@ -0,0 +1,13 @@
+import axios from "axios"
+import { getUserId } from "../utils/uuid"
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+})
+
+api.interceptors.request.use((config) => {
+  config.headers["anonymous-id"] = getUserId()
+  return config
+})
+
+export default api
